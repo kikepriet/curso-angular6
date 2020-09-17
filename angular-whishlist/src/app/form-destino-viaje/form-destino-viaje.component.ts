39193f00caf3fee53cdb6c9eb7bd4419d03284f9
Enrique Prieto <kikepriet@gmail.com>
2020-09-16 23:16:35 -0500

    regresando en el tiempo se rompió el código

---
+++ angular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DestinoViaje } from '../models/destino-viaje.model';
+
+@Component({
+  selector: 'app-form-destino-viaje',
+  templateUrl: './form-destino-viaje.component.html',
+  styleUrls: ['./form-destino-viaje.component.css']
+})
+export class FormDestinoViajeComponent implements OnInit {
+  @Output() onItemAdded: EventEmitter<DestinoViaje>;
+  fg: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.onItemAdded = new EventEmitter();
+    this.fg = fb.group({
+      nombre: [''],
+      url: ['']
+    });
+
+    // this.fg.valueChanges.subscribe((form: any) => {
+    //   console.log('cambió el formulario: ');
+    // })
+   }
+
+  ngOnInit(): void {
+  }
+
+  guardar(nombre: string, url: string): boolean {
+    const d = new DestinoViaje(nombre, url);
+    this.onItemAdded.emit(d);
+    return false;
+  }
+
+}
